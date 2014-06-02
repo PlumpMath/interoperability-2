36f780db7a0890ca99a621bda5e037db2d30f800
efrengarcial <efren.gl@gmail.com>
2014-06-02 18:50:38 -0500

    desa

--- src/main/java/org/wtf/interoperability/config/AppConfig.java
+++ src/main/java/org/wtf/interoperability/config/AppConfig.java
@@ -1,6 +1,5 @@
 package org.wtf.interoperability.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -9,12 +8,10 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("/WEB-INF/beans.xml")
 public class AppConfig {
 
-	private @Value("#{keystoreProperties['org.apache.ws.security.crypto.merlin.file']}") String file;
-	
 	@Bean(name="object")
 	public Object object(){		
-		System.out.println(file);
-		return file;
+		System.out.println("Inciando aplicacion............");
+		return new Object();
 	}
 	
 }
