a10694fc143ce85a5c2c03fab827431e15f624cc
efrengarcial <efren.gl@gmail.com>
2014-06-04 17:00:10 -0500

    desa

--- src/test/java/com/example/tutorial/ws/integration/CardFraudulentServiceTest.java
+++ src/test/java/com/example/tutorial/ws/integration/CardFraudulentServiceTest.java
@@ -9,7 +9,11 @@ import org.wtf.interoperability.ws.vo.RequiredHeaderMissingFault;
 import org.wtf.interoperability.ws.vo.SystemUnavailableFault;
 
 import com.aes.service.accounts.CardFraudulentService;
+import com.aes.service.accounts.model.CardStatusModRqType;
+import com.aes.service.accounts.model.ContextRqHdrType;
+import com.aes.service.accounts.model.ModCardStatusInp;
 import com.aes.service.accounts.model.ModCardStatusInput;
+import com.aes.service.accounts.model.MsgRqHdrType;
 
 @ContextConfiguration(locations = {        
         "classpath*:/META-INF/cxf/cxf.xml",
@@ -31,6 +35,17 @@ public class CardFraudulentServiceTest extends AbstractTestNGSpringContextTests{
 	public void basicTest() throws URISyntaxException, RequiredHeaderMissingFault, SystemUnavailableFault {
 		
 		ModCardStatusInput modCardStatusRequest = new ModCardStatusInput();
+		ModCardStatusInp value1 = new ModCardStatusInp();
+		modCardStatusRequest.setModCardStatusInp(value1 );
+		CardStatusModRqType value2 = new CardStatusModRqType();
+		value1.setCardStatusModRq(value2 );
+		value2.setRqUID("10000000-1111-2222-3333-000000000000");
+		MsgRqHdrType value3 = new MsgRqHdrType();
+		value2.setMsgRqHdr(value3 );
+		ContextRqHdrType value4 = new ContextRqHdrType();
+		value3.setContextRqHdr(value4 );
+		value4.setClientDt("2014-05-21T09:00:00");
+		
 		getPort().report(modCardStatusRequest);
 	}
 }
